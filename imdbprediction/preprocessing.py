c6297daabd2d2c0bbbd1f0b3c6574f488d69c4e3
Ondrej Kipila <ondrej.kipila@pixwell.sk>
2017-03-11 20:26:28 +0100

    Add boxplots, describe

--- imdbprediction/preprocessing.py
+++ imdbprediction/preprocessing.py
@@ -3,34 +3,51 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from matplotlib.figure import Figure
+
+
+def save_plot(plot, file_name):
+    fig = plot.get_figure() if not isinstance(plot, Figure) else plot
+    fig.savefig(file_name, bbox_inches='tight')
+
 
 sb.set(style='white')
+
 movies = pd.read_csv('data/movie_metadata.csv')
+num_data = movies.select_dtypes(include=[np.number])
+cat_data = movies.select_dtypes(exclude=[np.number])
+
+
+print(movies.describe())
+
+
+# Boxplots
+for col in num_data.columns:
+    _, ax = plt.subplots(figsize=(11, 9))
+    plot = sb.boxplot(x=num_data[col], ax=ax)
+    save_plot(plot, 'box_{}.png'.format(col))
+
+
+
+# Histograms
+for col in cat_data.columns:
+    plot = cat_data[col].value_counts().plot(kind='bar')
+    save_plot(plot, 'hist_{}.png'.format(col))
 
 
 # Missing values
-_, ax = plt.subplots(figsize=(11, 9))
-
 sb.set_color_codes('pastel')
 plot = sb.barplot(x=movies.count(), y=movies.columns, color='b')
-
 sb.set_color_codes('muted')
 plot = sb.barplot(x=movies.isnull().sum(), y=movies.columns, color='b')
-
-fig = plot.get_figure()
-fig.savefig('null.png', bbox_inches='tight')
+save_plot(plot, 'null.png')
 
 
 # Correlation
 corr = movies.corr()
-
 mask = np.zeros_like(corr, dtype=np.bool)
 mask[np.triu_indices_from(mask)] = True
-
-_, ax = plt.subplots(figsize=(11, 9))
-
 cmap = sb.diverging_palette(220, 10, as_cmap=True)
-
+_, ax = plt.subplots(figsize=(11, 9))
 plot = sb.heatmap(corr, mask=mask, cmap=cmap, ax=ax, linewidths=0.5, cbar_kws={'shrink': .5})
-fig = plot.get_figure()
-fig.savefig('corr.png', bbox_inches='tight')
+save_plot(plot, 'corr.png')
